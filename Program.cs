12eb24bacb80f6cb2ea67f8b723fd0672639bb30
Patricia Boian <patyboian86@gmail.com>
2025-11-26 11:54:16 -0300

    iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if(loginDTO.Email == "admin@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com sucesso");
+    else 
+        return Results.Unauthorized();
+});
+
+
+app.Run();
+
+    public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
